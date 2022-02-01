e927277d9b6372a14a950c63ce933650ef185500
MURAGESH NAD <42669128+murageshnad@users.noreply.github.com>
2022-02-01 20:08:17 +0530

    Develop (#2)
    * completed the flow
    
    * completed the flow
    
    Co-authored-by: muragesh4444 <muragesh.k@vuedata.in>

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello Node!\n';
   res.end(msg);
 });
 
 server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+  console.log(`Server running on ${port}/`);
 });
